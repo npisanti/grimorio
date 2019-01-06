a1d368d04cd30415e6a7e7cd992a1bb1e9084189
Nicola Pisanti <nicola@npisanti.com>
2019-01-07 00:18:59 +0100

    minor tweaks

--- 07_private.sh
+++ 07_private.sh
@@ -2,16 +2,15 @@
 
 # private repos
 cd ~/oF/apps
-git clone https://gitlab.com/npisanti/artefatti.git
 git clone https://gitlab.com/npisanti/miscela.git
 git clone https://gitlab.com/npisanti/np-sketches.git sketches
 git clone https://gitlab.com/npisanti/np-stubs.git stubs
-git clone https://gitlab.com/npisanti/scrittura.git
 git clone https://gitlab.com/npisanti/np-instruments.git
 
 mkdir -p ~/workspace
 cd ~/workspace
 git clone https://gitlab.com/npisanti/graphicsrepo.git graphics
 git clone https://gitlab.com/npisanti/bashcode.git
+
 
 exit
---
+++ 08_projects.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# private projects 
+mkdir -p ~/oF/apps/artefatti
+cd ~/oF/apps/artefatti
+git clone https://gitlab.com/npisanti/scrittura.git
+git clone https://gitlab.com/npisanti/junk-rituals.git
+git clone https://gitlab.com/npisanti/quattro.git
+git clone https://gitlab.com/npisanti/cinque.git
+
+exit
