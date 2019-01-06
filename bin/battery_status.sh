fc9bddb8dc503857e9da60f7019b4b5b3370e97a
Nicola Pisanti <nicola@npisanti.com>
2019-01-06 19:38:30 +0100

    fixes battery status script

--- bin/battery_status.sh
+++ bin/battery_status.sh
@@ -3,7 +3,7 @@
 ETA=$( acpi -b | awk '{print $5}' | sed 's/,//g' )
 STATUS=$( acpi -b | awk '{print $3 " " $4}' | sed 's/,//g' )
 
-if [ "$ETA" == "discharging" ]
+if [ "$ETA" == "discharging" ] || [ "$ETA" == "100%" ];
 then
   STATUS="AC / Full"
 fi
