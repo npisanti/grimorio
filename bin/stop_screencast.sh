187c10595258c2621856520610d029ef4d79a9ba
Nicola Pisanti <nicola@npisanti.com>
2019-01-10 00:40:27 +0100

    improved stop screencast

--- bin/stop_screencast.sh
+++ bin/stop_screencast.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+VIDEO=$( pgrep ffmpeg )
 AUDIO=$( pgrep jack_capture )
 
-if [ "$AUDIO" != "" ] ; then
+if [ "$VIDEO" != "" ] && [ "$AUDIO" != "" ]; then
     killall ffmpeg 
     killall jack_capture
     sleep 2
     DATE=`date +%Y%m%d`
     TIME=`date +%Hh%M`
     ffmpeg -i ~/Videos/screencast_temp.mkv -i ~/Videos/screencast_temp.wav -c:v copy -c:a libvorbis -shortest ~/Videos/screencast_$DATE-$TIME.mkv
-else
+elif [ "$VIDEO" != "" ] ; then
     killall ffmpeg 
     sleep 2
     DATE=`date +%Y%m%d`
     TIME=`date +%Hh%M`    
     mv ~/Videos/screencast_temp.mkv ~/Videos/screencast_$DATE-$TIME.mkv
+elif [ "$AUDIO" != "" ] ; then
+    killall jack_capture 
+    sleep 2
+    DATE=`date +%Y%m%d`
+    TIME=`date +%Hh%M`    
+    mv ~/Videos/screencast_temp.wav ~/Videos/jack_recording_$DATE-$TIME.wav
 fi
 
 exit
