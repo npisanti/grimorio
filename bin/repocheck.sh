01348b5703ae12fe0d79fb8cefd64d4c08bbc775
Nicola Pisanti <nicola@npisanti.com>
2019-01-10 21:13:40 +0100

    repocheck report to push

--- bin/repocheck.sh
+++ bin/repocheck.sh
@@ -31,7 +31,9 @@ for i in "${paths[@]}"
 do
     cd "$HOME/$i"
     status=`git status | sed -n 3p`
-    if [ "$status" != "nothing to commit, working tree clean" ]; then
+    if [ "$status" == "  (use \"git push\" to publish your local commits)" ]; then
+        echo "> $i"
+    elif [ "$status" != "nothing to commit, working tree clean" ]; then
         echo "$i"
         nothingtocommit="false"
     fi
