1e66841ea39821f1649ab6e1758bb1bfd23d1c33
Nicola Pisanti <nicola@npisanti.com>
2019-01-20 19:03:04 +0100

    ardour build script

---
+++ 12_build_ardour.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ~/apps
+
+cd ~/apps
+git clone git://github.com/Ardour/ardour.git
+
+sudo apt-get install -y libglibmm-2.4-dev libcurl4-gnutls-dev liblo-dev libtag1-dev vamp-plugin-sdk librubberband-dev libaubio-dev liblrdf0-dev libsamplerate0-dev lv2-dev libserd-dev libsord-dev libsratom-dev liblilv-dev libgtkmm-2.4-dev libarchive-dev libfftw3-dev
+
+cd ~/apps/ardour
+
+./waf configure
+
+./waf
+
+exit
