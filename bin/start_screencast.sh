83d3a7e671d5051270135883e9565254fb30d8ff
Nicola Pisanti <nicola@npisanti.com>
2019-04-28 11:30:22 +0200

    some code for centered 600x720 screencast

--- bin/start_screencast.sh
+++ bin/start_screencast.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#ffmpeg -y -f x11grab -s 600x720 -i :0.0+660,140 -vcodec libx264 -threads 2 $HOME/Videos/screencast_temp.mkv &
 ffmpeg -y -f x11grab -s 1920x1080 -i :0.0 -vcodec libx264 -threads 2 $HOME/Videos/screencast_temp.mkv &
 jack_capture -d 6000 $HOME/Videos/screencast_temp.wav &
 
