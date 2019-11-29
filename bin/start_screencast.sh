592a488282d8e60d5e54424433d4f8f3cffd71e1
Nicola Pisanti <nicola@npisanti.com>
2019-11-29 17:40:09 +0100

    1280x720 screencast

--- bin/start_screencast.sh
+++ bin/start_screencast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ffmpeg -y -f x11grab -s 600x720 -i :0.0+660,140 -vcodec libx264 -threads 2 $HOME/Videos/screencast_temp.mkv &
-ffmpeg -y -f x11grab -s 1280x200 -i :0.0+320,35 -vcodec libx264 -threads 2 $HOME/Videos/screencast_temp.mkv &
+ffmpeg -y -f x11grab -s 1280x720 -i :0.0+320,135 -vcodec libx264 -threads 2 $HOME/Videos/screencast_temp.mkv &
 jack_capture -d 6000 $HOME/Videos/screencast_temp.wav &
 
 exit
